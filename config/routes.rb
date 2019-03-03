a512b7eb63d25b343329316ceb53e011aa2f082c
valeria campilongo <cvaleriac@hotmail.it>
2019-03-03 23:09:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new, :create]
+  resources :students, only: :index
+  get '/student/:id', to: 'students#show', as: 'student'
+  post '/students', to: 'students#create', as: 'create_student'
+  get '/students/new', to: 'students#new', as: 'new_student'
 end
